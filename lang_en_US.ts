3a8c37ce7b518e16fd6048b1bfb29cb39e8ee18c
Андрей <161771621+Catfish270@users.noreply.github.com>
2025-06-09 18:30:54 +0300

    Add files via upload

---
+++ lang_en_US.ts
@@ -0,0 +1,110 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="19"/>
+        <source>MainWindow</source>
+        <translation>Humidity Monitor</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="60"/>
+        <source>Показатель влажности (%)</source>
+        <translation>Humidity Level (%)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="108"/>
+        <source>Минимальный порог</source>
+        <translation>Lower Threshold</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="127"/>
+        <source>40</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="160"/>
+        <source>Максимальный порог</source>
+        <translation>Upper Threshold</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="181"/>
+        <source>60</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="235"/>
+        <source>Установить пороговые значения</source>
+        <translation>Set Threshold Values</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="248"/>
+        <source>Язык программы:</source>
+        <translation>Program language:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="269"/>
+        <location filename="mainwindow.cpp" line="96"/>
+        <source>Русский</source>
+        <translation>Русский</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="282"/>
+        <location filename="mainwindow.cpp" line="97"/>
+        <source>English</source>
+        <translation>English</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="295"/>
+        <location filename="mainwindow.cpp" line="98"/>
+        <source>Deutsch</source>
+        <translation>Deutsch</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="317"/>
+        <source>toolBar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="94"/>
+        <source>Humidity Monitor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="103"/>
+        <location filename="mainwindow.cpp" line="268"/>
+        <source>Некорректное значение нижней границы.</source>
+        <translation>Invalid lower threshold value.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="106"/>
+        <location filename="mainwindow.cpp" line="274"/>
+        <source>Некорректное значение верхней границы.</source>
+        <translation>Invalid upper threshold value.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="109"/>
+        <location filename="mainwindow.cpp" line="279"/>
+        <source>Указан некорректный диапазон.</source>
+        <translation>Invalid range specified.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="112"/>
+        <location filename="mainwindow.cpp" line="308"/>
+        <source>Диапазон успешно обновлен.</source>
+        <translation>Range updated successfully.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="131"/>
+        <location filename="mainwindow.cpp" line="231"/>
+        <source>Failed to open file:</source>
+        <translation>Failed to open file:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="299"/>
+        <source>Failed to save file</source>
+        <translation>Failed to save file</translation>
+    </message>
+</context>
+</TS>
---
+++ lang_ru_RU.ts
@@ -0,0 +1,110 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="19"/>
+        <source>MainWindow</source>
+        <translation>Монитор влажности</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="60"/>
+        <source>Показатель влажности (%)</source>
+        <translation>Показатель влажности (%)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="108"/>
+        <source>Минимальный порог</source>
+        <translation>Минимальный порог</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="127"/>
+        <source>40</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="160"/>
+        <source>Максимальный порог</source>
+        <translation>Максимальный порог</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="181"/>
+        <source>60</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="235"/>
+        <source>Установить пороговые значения</source>
+        <translation>Установить пороговые значения</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="248"/>
+        <source>Язык программы:</source>
+        <translation>Язык программы:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="269"/>
+        <location filename="mainwindow.cpp" line="96"/>
+        <source>Русский</source>
+        <translation>Русский</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="282"/>
+        <location filename="mainwindow.cpp" line="97"/>
+        <source>English</source>
+        <translation>English</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="295"/>
+        <location filename="mainwindow.cpp" line="98"/>
+        <source>Deutsch</source>
+        <translation>Deutsch</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="317"/>
+        <source>toolBar</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="94"/>
+        <source>Humidity Monitor</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="103"/>
+        <location filename="mainwindow.cpp" line="268"/>
+        <source>Некорректное значение нижней границы.</source>
+        <translation>Некорректное значение нижней границы.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="106"/>
+        <location filename="mainwindow.cpp" line="274"/>
+        <source>Некорректное значение верхней границы.</source>
+        <translation>Некорректное значение верхней границы.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="109"/>
+        <location filename="mainwindow.cpp" line="279"/>
+        <source>Указан некорректный диапазон.</source>
+        <translation>Указан некорректный диапазон.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="112"/>
+        <location filename="mainwindow.cpp" line="308"/>
+        <source>Диапазон успешно обновлен.</source>
+        <translation>Диапазон успешно обновлен.</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="131"/>
+        <location filename="mainwindow.cpp" line="231"/>
+        <source>Failed to open file:</source>
+        <translation>Ошибка открытия файла:</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="299"/>
+        <source>Failed to save file</source>
+        <translation>Ошибка сохранения файла</translation>
+    </message>
+</context>
+</TS>
